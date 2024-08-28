053c73ab1b6828285d9844db7b76842030bed2ac
Ashish Singh <itstheashish@gmail.com>
2024-08-28 21:09:25 +0530

    added support for path params

--- src/main/java/com/codingshuttle/anujTutorial/tutorial/controllers/EmployeeController.java
+++ src/main/java/com/codingshuttle/anujTutorial/tutorial/controllers/EmployeeController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.codingshuttle.anujTutorial.tutorial.dto.EmployeeDTO;
 
+import jakarta.websocket.server.PathParam;
+
 import java.time.LocalDate;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,13 +24,27 @@ import org.springframework.web.bind.annotation.PathVariable;
 @RestController
 public class EmployeeController {
 
+    // url : http://localhost:8080/employees
     @GetMapping(path = "/employees")
     public EmployeeDTO getEmployee() {
         return new EmployeeDTO(12, "Anuj", LocalDate.of(2024, 8, 28), true);
     }    
 
+    // url : http://localhost:8080/employees/90000    -    Path variables
     @GetMapping(path = "/employees/{id}")
     public EmployeeDTO getEmployeeById(@PathVariable("id") long id) {
         return new EmployeeDTO(id, "Anuj", LocalDate.of(2024, 8, 28), true);
-    }    
+    }
+    
+    // url : http://localhost:8080/sortemployees?sortBy=age         -    Path Params
+    @GetMapping(path = "/sortemployees")
+    public String getEmployeePathParam(@PathParam("sortBy") String sortBy) {
+        if(sortBy == null ){
+            return "No sorting order mentioned";       //  will print No sorting order mentioned
+        }else {
+            return "Sort the given field by " + sortBy ;       //  will print Sort the given field by (age/whatever is passed as the path param)
+        }
+    }
+
+
 }
